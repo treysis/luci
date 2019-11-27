bd7cbdaeaf17841bbc344541372a7bed7f686b84
treysis <treysis@gmx.net>
2019-11-27 22:10:44 +0100

    Allow setting of multiple IPv6 address and prefix in LuCi.

--- protocols/luci-proto-ipv6/luasrc/model/cbi/admin_network/proto_6in4.lua
+++ protocols/luci-proto-ipv6/luasrc/model/cbi/admin_network/proto_6in4.lua
@@ -24,16 +24,17 @@ peeraddr.datatype = "ip4addr"
 
 ip6addr = s:taboption("general", Value, "ip6addr",
 	translate("Local IPv6 address"),
-	translate("This is the local endpoint address assigned by the tunnel broker, it usually ends with <code>:2</code>"))
+	translate("This is the local endpoint address assigned by the tunnel broker, it usually ends with <code>...:2/64</code>"))
 
 ip6addr.datatype = "ip6addr"
 
 
-local ip6prefix = s:taboption("general", Value, "ip6prefix",
+local ip6prefix = s:taboption("general", DynamicList, "ip6prefix",
 	translate("IPv6 routed prefix"),
 	translate("This is the prefix routed to you by the tunnel broker for use by clients"))
 
 ip6prefix.datatype = "ip6addr"
+ip6prefix.cast = "string"
 
 
 local update = section:taboption("general", Flag, "_update",
